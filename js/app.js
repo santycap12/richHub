44436748bc6b05abf1897fa53f0b681c3d045f0b
Santiago Capuano <santycap2@gmail.com>
2023-06-07 16:32:16 -0300

    bandera de argentina

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 const caja = document.querySelector('#caja');
+const caja2 = document.createElement('div');
+const caja3 = document.createElement('div');
+caja2.id = "caja2";
+caja3.id = "caja3";
 
 caja.addEventListener('click', () => {
-    const caja2 = document.createElement('div');
-    caja2.classList.add('caja2');
     caja.appendChild(caja2);
+    caja2.addEventListener('click', () => {
+        caja2.appendChild(caja3);
+    });
 });
 
 caja.addEventListener('dblclick', limpiarHTML);
 
 function limpiarHTML() {
     while (caja.firstChild) {
+        while (caja2.firstChild) {
+            caja2.removeChild(caja2.firstChild);
+        };
         caja.removeChild(caja.firstChild);
     };
 };
